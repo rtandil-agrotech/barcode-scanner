4519ea6aab2ed822fd6f47dd06ee5ec05592bbb9
Riko Tan <riktandil@gmail.com>
2025-08-15 02:15:45 +0700

    fix: remove esc key to quit

--- main.py
+++ main.py
@@ -23,21 +23,16 @@ def on_press(key):
     except Exception as e:
         print(f"Error: {e}")
 
-
-def on_release(key):
-    if key == keyboard.Key.esc:
-        print("\n[Exiting]")
-        return False
-
-
 # Start listener
+
+
 def main():
     global mqtt_con
     mqtt_con = setup_mqtt()
     mqtt_con.loop_start()
 
-    with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
-        print("Waiting for scanner input... (Press ESC to quit)")
+    with keyboard.Listener(on_press=on_press) as listener:
+        print("Waiting for scanner input...")
         listener.join()
 
 
